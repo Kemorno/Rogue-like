43d31586995f787aa2b3d1a23df8aead11467f92
Kemorno <kemorno@gmail.com>
2018-08-14 16:57:34 -0300

    Improved most classes
    removed Tuples, improved Rooms and created a new class to indetify tiles.

--- Assets/LevelCreator/Scripts/Resources.cs
+++ Assets/LevelCreator/Scripts/Resources.cs
@@ -1,97 +1,161 @@
+using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 namespace Resources
 {
-    public class Tuple<T1, T2>
+    public class Room
     {
-        public T1 First { get; private set; }
-        public T2 Second { get; private set; }
+        public int RoomID;
+        public Tile CenterTile = null;
+        public List<Tile> FloorTiles = new List<Tile>();
+        public List<Tile> WallTiles = new List<Tile>();
 
-        public Tuple(T1 _First, T2 _Second)
+        public Room(int _RoomID)
         {
-            First = _First;
-            Second = _Second;
+            RoomID = _RoomID;
+        }
+
+        #region Operators
+        public static bool operator==(Room obj1, Room obj2)
+        {
+            return obj1.Equals(obj2);
+        }
+        public static bool operator!=(Room obj1, Room obj2)
+        {
+            return !obj1.Equals(obj2);
+        }
+        public override bool Equals(object other)
+        {
+            if (other == null) return false;
+            Room obj = other as Room;
+
+            return (RoomID == obj.RoomID
+                && FloorTiles == obj.FloorTiles
+                && WallTiles == obj.WallTiles
+                && CenterTile == obj.CenterTile);
         }
         public override int GetHashCode()
         {
             unchecked
             {
-                int hash = 0;
-
-                hash = (First.GetHashCode() << 16) ^ (Second.GetHashCode() << 8);
-
-                return hash;
+                return (FloorTiles.GetHashCode() * 17 + WallTiles.GetHashCode());
             }
         }
-        public static bool operator==(Tuple<T1, T2> obj1, Tuple<T1, T2> obj2)
+        public override string ToString()
         {
-            if (ReferenceEquals(null, obj2))
-                return false;
-            return (obj1.GetHashCode() == obj2.GetHashCode());
+            return ("ID:" + RoomID + " ###Total Tile Count:" + (FloorTiles.Count+ WallTiles.Count) +
+                "\nFloor Tile Count:" + FloorTiles.Count +
+                " ###Wall Tile Count:" + WallTiles.Count);
         }
-        public static bool operator!=(Tuple<T1, T2> obj1, Tuple<T1, T2> obj2)
-        {
-            if (ReferenceEquals(null, obj2))
-                return true;
-            return !(obj1.GetHashCode() == obj2.GetHashCode());
-        }
-        public bool Equals(Tuple<T1, T2> other)
-        {
-            if (other == null)
-                return false;
-
-            if (GetHashCode() == other.GetHashCode())
-                return true;
-            else
-                return false;
-
-        }
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            Tuple<T1, T2> other = obj as Tuple<T1, T2>;
-
-            return obj.GetType() == GetType() && Equals(other);
-        }
+        #endregion
     }
-    public static class Tuple
-    {
-        public static Tuple<T1, T2> New<T1, T2>(T1 first, T2 second)
-        {
-            var tuple = new Tuple<T1, T2>(first, second);
-            return tuple;
-        }
-    }
-
     public class Tile
     {
         public int RoomId = -1;
-        public Vector2Int Coord = Vector2Int.zero;
+        public CoordInt Coord;
         public Enums.tileType Type = Enums.tileType.Wall;
         public Enums.roomClass Class = Enums.roomClass.Neutral;
         public bool walkable = false;
 
-        public Tile(Vector2Int _Coord)
+        #region Constructors
+        public Tile(CoordInt _Coord)
         {
             Coord = _Coord;
         }
-        public Tile(Vector2Int _Coord, Enums.tileType _Type)
+        public Tile(CoordInt _Coord, Enums.tileType _Type)
         {
             Coord = _Coord;
             Type = _Type;
             walkable = (_Type == Enums.tileType.Floor) ? true : false;
         }
+        #endregion
 
         public string Info()
         {
             return "Tile from Room #" + RoomId + " at Coords " + Coord.ToString() + " is a " + Type.ToString() + " and belongs to the " + Class.ToString() + " Class." + ((walkable) ? " You CAN walk on it" : " You CANNOT walk on it");
         }
+    }
+    public class CoordInt
+    {
+        public int X;
+        public int Y;
+
+        #region Constructors
+        public CoordInt(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        #endregion
+
+        public override int GetHashCode()
+        {
+            return new Tuple<int, int>(x, y).GetHashCode();
+        }
+        public override bool Equals(object other)
+        {
+            if (other == null) return false;
+
+            CoordInt obj = other as CoordInt;
+
+            return (X == obj.X
+                && Y == obj.Y);
+        }
+        public Vector2Int GetVector2Int()
+        {
+            return new Vector2Int(X, Y);
+        }
+        public override string ToString()
+        {
+            return (X + "," + Y);
+        }
+        public Tuple<int, int> GetTuple()
+        {
+            return new Tuple<int, int>(x, y);
+        }
+
+        #region Operators
+        public static bool operator==(CoordInt obj1, CoordInt obj2)
+        {
+            return (obj1.Equals(obj2));
+        }
+        public static bool operator!=(CoordInt obj1, CoordInt obj2)
+        {
+            return !(obj1.Equals(obj2));
+        }
+        public static bool operator<(CoordInt obj1, CoordInt obj2)
+        {
+            return (obj1.X < obj2.X && obj1.Y < obj2.Y);
+        }
+        public static bool operator >(CoordInt obj1, CoordInt obj2)
+        {
+            return (obj1.X > obj2.X && obj1.Y > obj2.Y);
+        }
+        public static bool operator <=(CoordInt obj1, CoordInt obj2)
+        {
+            return (obj1.X <= obj2.X && obj1.Y <= obj2.Y);
+        }
+        public static bool operator >=(CoordInt obj1, CoordInt obj2)
+        {
+            return (obj1.X >= obj2.X && obj1.Y >= obj2.Y);
+        }
+        public static CoordInt operator+(CoordInt obj1, CoordInt obj2)
+        {
+            return new CoordInt(obj1.X + obj2.X, obj1.Y + obj2.Y);
+        }
+        public static CoordInt operator-(CoordInt obj1, CoordInt obj2)
+        {
+            return new CoordInt(obj1.X - obj2.X, obj1.Y - obj2.Y);
+        }
+        public static CoordInt operator*(CoordInt obj1, CoordInt obj2)
+        {
+            return new CoordInt(obj1.X * obj2.X, obj1.Y * obj2.Y);
+        }
+        public static CoordInt operator/(CoordInt obj1, CoordInt obj2)
+        {
+            return new CoordInt(obj1.X / obj2.X, obj1.Y / obj2.Y);
+        }
+        #endregion
     }
 }
