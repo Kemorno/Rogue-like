6e572646feffef397af4d432a0aa0096f2c77543
Kemorno <kemorno@gmail.com>
2019-03-22 12:29:51 -0300

    New Controllers

---
+++ Assets/LevelCreator/Scripts/MobController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Resources;
+using Enums;
+
+public class MobController : MonoBehaviour
+{
+    public Mob mob;
+
+    private void Awake()
+    {
+        mob = new Mob(0, 4, MobType.Neutral);
+    }
+}
