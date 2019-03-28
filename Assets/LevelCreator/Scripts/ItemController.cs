fed36b357be97dbb504a67000c679a6631018df1
Kemorno <kemorno@gmail.com>
2019-03-28 17:04:39 -0300

    Update ItemController.cs

--- Assets/LevelCreator/Scripts/ItemController.cs
+++ Assets/LevelCreator/Scripts/ItemController.cs
@@ -7,4 +7,15 @@ using Enums;
 public class ItemController : MonoBehaviour
 {
     public Item item;
+    public Sprite Sprite;
+
+    private void Awake()
+    {
+        item = new Item();
+        item.isConsumable(false);
+        item.SetSprite(Sprite);
+        gameObject.AddComponent<SpriteRenderer>();
+        gameObject.GetComponent<SpriteRenderer>().sprite = Sprite;
+        gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Items";
+    }
 }
