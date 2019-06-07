c97f100b5def6532f6152458bde5a02ee9f66c01
Pedro Costa <pedro.costa@localiza.com>
2019-06-07 13:57:22 -0300

    Create ProjectileHandler.cs

---
+++ Assets/LevelCreator/Scripts/ProjectileHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProjectileHandler
+{
+    public static Vector2 GetDirection(Vector2 ThrowerPos, Vector2 TargetPos, float Deviation = 1f)
+    {
+        Vector2 Direction = Vector2.zero;
+
+        Direction = TargetPos - ThrowerPos;
+
+        return GetDeviation(Direction, Deviation);
+    }
+
+    public static Vector2 GetVelocityMultiplier(Vector2 ThrowerPos, Vector2 TargetPos)
+    {
+        Vector2 Direction = Vector2.zero;
+
+        Direction = TargetPos - ThrowerPos;
+
+        return Direction.normalized;
+    }
+
+    public static Vector2 GetForce(Vector2 ThrowerPos, Vector2 TargetPos, float Deviation = 1f)
+    {
+        Vector2 Direction = Vector2.zero;
+
+        Direction = TargetPos - ThrowerPos;
+
+        return GetDeviation(Direction, Deviation).normalized;
+    }
+
+    public static Vector2 GetForce(Vector2 ThrowerPos, Vector2 TargetPos, float forceCap, float Deviation = 1f)
+    {
+        Vector2 Direction = Vector2.zero;
+
+        Direction = TargetPos - ThrowerPos;
+
+        if (Direction.x > forceCap)
+            Direction.x = forceCap;
+        if (Direction.y > forceCap)
+            Direction.y = forceCap;
+
+        return GetDeviation(Direction, Deviation);
+    }
+
+    private static Vector2 GetDeviation(Vector2 V2, float Deviation)
+    {
+        Vector2 Direction = V2;
+
+        Direction.x += UnityEngine.Random.Range(-Deviation / 2f, Deviation / 2f);
+        Direction.y += UnityEngine.Random.Range(-Deviation / 2f, Deviation / 2f);
+
+        return Direction;
+    }
+}
+
